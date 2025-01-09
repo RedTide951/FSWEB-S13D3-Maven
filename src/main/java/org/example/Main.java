90f358bcbb07728c9a4b74fb8dd34637196dda59
Oğuzhan Bütüner <oguzhanraifbutuner@gmail.com>
2025-01-09 15:36:19 +0300

    start

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,33 @@
 package org.example;
 
 public class Main {
+
+    class Person {
+        String firstName;
+        String lastName;
+        int age;
+        String job;
+        boolean isEmployed;
+        boolean isDeveloper;
+
+
+        public Person(String firstName,String lastName, int age) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+        public Person(String firstName,String lastName, int age, String job, boolean isEmployed, boolean isDeveloper) {
+            this(firstName, lastName, age);
+            this.job = job;
+            this.isEmployed = isEmployed;
+            this.isDeveloper = isDeveloper;
+        }
+
+    }
+
+
+
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
